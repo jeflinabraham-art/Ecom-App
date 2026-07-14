6a82ec25774e2df43c8320412f3084113dc8dec9
jeflinAbraham <jeflinabraham21@gmail.com>
2026-07-14 22:42:21 +0530

    timezone fix

--- src/main/java/com/app/ecom/EcomAppApplication.java
+++ src/main/java/com/app/ecom/EcomAppApplication.java
@@ -2,11 +2,14 @@ package com.app.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class EcomAppApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(EcomAppApplication.class, args);
 	}
 
